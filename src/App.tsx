0cbb3b15b747ccce5fa856820e06b8065f975a9d
MaximKornienkou <maxim.kornienkou@gmail.com>
2021-12-09 21:26:20 +0200

    add loginPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import {LoginPage} from './components/LoginPage/LoginPage';
 import {Paths} from './routes/paths';
 
 function App() {
+
     const {loginPage} = Paths;
+
     return (
         <div>
             <h2>Start</h2>
             <Routes>
                 <Route path={loginPage} element={<LoginPage/>}/>
             </Routes>
-
         </div>
-
     );
 }
 
